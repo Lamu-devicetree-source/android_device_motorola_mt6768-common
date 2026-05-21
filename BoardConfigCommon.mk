ab8d599221df2e2a8564c617cc912b0576c1c5ac
Ben Grisdale <bengris32@protonmail.ch>
2026-05-21 13:24:44 +0200

    mt6768-common: Set SELinux permissive for bringup
    Change-Id: I0aa3af90e841b3706797ce64575a1148e8951591

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 
 # Bootloader
 BOARD_VENDOR := motorola
