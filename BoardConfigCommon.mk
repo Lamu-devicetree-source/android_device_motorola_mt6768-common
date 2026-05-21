75e69989b57ed21bb8efb8b42bfdca789e913d06
bengris32 <bengris32@protonmail.ch>
2026-05-21 12:13:26 +0200

    mt6768-common: Define Architecture flags
    Change-Id: If12b79122e1817d6f3e7acb53a8db47123b175d9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,5 +5,13 @@
 
 COMMON_PATH := device/motorola/mt6768-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a75
+
 # Inherit the proprietary files
 include vendor/motorola/mt6768-common/BoardConfigVendor.mk
