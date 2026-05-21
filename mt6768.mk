39d2204bfb716a0940068751c517afb5a18dc4aa
R0rt1z2 <roger@r0rt1z2.com>
2026-05-21 13:23:16 +0200

    mt6768-common: Set Boot Image flags
    Change-Id: I8f6a7d3031b7fa36602dd532c0014c302a97c21f
    Signed-off-by: R0rt1z2 <roger@r0rt1z2.com>

--- mt6768.mk
+++ mt6768.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit generic_ramdisk product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Platform
 TARGET_BOARD_PLATFORM := mt6768
 
