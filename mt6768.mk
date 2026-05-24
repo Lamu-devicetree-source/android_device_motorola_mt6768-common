f0c96ff932477d71b2cb57782a4603bf95614c89
R0rt1z2 <roger@r0rt1z2.com>
2026-05-24 19:11:06 +0200

    mt6768-common: Inherit default dalvik heap configuration
    Since we'll have models with 3GB, 4GB, and even 8GB of RAM,
    this will be handled inside libinit in the future.
    
    Change-Id: Iffd1d2714e1595737c9c9cbda8c84159f1a20361
    Signed-off-by: R0rt1z2 <roger@r0rt1z2.com>

--- mt6768.mk
+++ mt6768.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
